3038eff38e2da3b488796f24bb78dbc157c08ded
Andydenike <denikeandrew@gmail.com>
2018-12-12 13:36:29 -0800

    Stripped class OrderedDict as empty template with pass

--- ordered_dict.py
+++ ordered_dict.py
@@ -1,13 +1,2 @@
 class OrderedDict:
-    def __init__(self):
-        self._keys = []
-        self._values = []
-
-    def keys(self):
-        pass
-
-    def values(self):
-        pass
-
-    def items(self):
-        pass
+    pass
